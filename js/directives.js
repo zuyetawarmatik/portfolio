1bb8cf56f4afdf7adc60fdeefa9389f454619d19
zuyetawarmatik <francesc.zuyet@gmail.com>
2014-08-16 17:01:55 +0800

    Back manipulation

--- js/directives.js
+++ js/directives.js
@@ -19,19 +19,40 @@
 	    			var mainColor = colors.mainColor;
 	    			var darkColor = colors.darkColor;
 	    			
+	    			var bodyWidth = $('body').width();
+    				var bodyHeight = $('body').height();
+    				var delta = bodyHeight / scope.tan;
+	    			
+    				var edgea1 = bodyWidth/2 + delta * 0.25;
+    				var edgea2 = bodyWidth/2 - delta * 0.75;
+    				
+    				var edgea1f = edgea1 - 10;
+    				var edgea2f = edgea2 - 10; var ledgea2f = bodyWidth - edgea2f;
+    				
 	    			if (scope.overlayPoly === undefined || isResize === true) {
 	    				scope.canvas.clear();
-	    				var bodyWidth = $('body').width();
-	    				var bodyHeight = $('body').height();
-	    				var delta = bodyHeight / scope.tan;
 	    				
-	    				var backPolyStr = '0,0 {0},0 {0},{1}, 0,{1}'.format(bodyWidth, bodyHeight);
+	    				var backPolyStr = '{0},0 {2},0 {2},{3}, {1},{3}'.format(edgea1f, edgea2f, bodyWidth, bodyHeight);
 	    				scope.backPoly = scope.canvas.polygon(backPolyStr).fill(darkColor);
-	    				var overlayPolyStr = '0,0 {0},0 {1},{2}, 0,{2}'.format(bodyWidth/2 + delta * 0.25, bodyWidth/2 - delta * 0.75, bodyHeight);
+	    				
+	    				var overlayPolyStr = '0,0 {0},0 {1},{2}, 0,{2}'.format(edgea1, edgea2, bodyHeight);
 	    				scope.overlayPoly = scope.canvas.polygon(overlayPolyStr).fill(mainColor);
 	    			} else {
-	    				scope.overlayPoly.animate(500, '>').attr({ fill: mainColor });
+	    				var backPolyStr = '{0},0 {2},0 {2},{3}, {1},{3}'.format(edgea1f + ledgea2f, bodyWidth, bodyWidth + ledgea2f, bodyHeight);
+	    				var newBackPoly = scope.canvas.polygon(backPolyStr).fill(darkColor);
 	    				scope.backPoly.animate(500, '>').attr({ fill: darkColor });
+	    				newBackPoly.animate(500, '>').move(edgea2f, 0).after(function() {
+		    				scope.backPoly.remove();
+		    				scope.backPoly = newBackPoly;
+	    				});
+	    				
+	    				var overlayPolyStr = '{0},0 0,0 {1},{2}, {0},{2}'.format(-edgea1, edgea2 - edgea1, bodyHeight);
+	    				var newOverlayPoly = scope.canvas.polygon(overlayPolyStr).fill(mainColor);
+	    				scope.overlayPoly.animate(500, '>').attr({ fill: mainColor });
+	    				newOverlayPoly.animate(500, '>').move(0, 0).after(function() {
+		    				scope.overlayPoly.remove();
+		    				scope.overlayPoly = newOverlayPoly;
+	    				});
 	    			}
 	    		};
 	    		
@@ -41,22 +62,11 @@
 	    			scope.redrawBackground();
 	    		});
 	    		
-	    		scope.$watch(function(){return $window.outerWidth;}, function(nv, ov) {
-	    			if (nv != ov) scope.redrawBackground(true);
-	    		});
-	    		
-	    		scope.$watch(function(){return $window.outerHeight;}, function(nv, ov) {
-	    			if (nv != ov) scope.redrawBackground(true);
-	    		});
-
 	    		window.bind('resize', function() {
+	    			scope.redrawBackground(true);
 	    			scope.$apply();
     		    });
 	    	}
 	    };
 	}]);
-	
-	appDirectives.directive('portNavScroll', ['$window', function($window) {
-	}]);
-	}
 })();
