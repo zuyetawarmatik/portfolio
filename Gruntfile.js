a56696aef3045aee72076bd04c05dc0cc31c8392
Bui Phuc Duyet <duyet.bui@forbes-digital.com>
2014-09-08 19:50:41 +0800

    Uglify JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,8 +61,11 @@ module.exports = function (grunt) {
 		},
 
 		uglify: {
+			options: {
+				mangle: false
+    	},
 			js: {
-				src: '<%= concat.portfolio.dest %>',
+				src: '<%= concat.js.dest %>',
 				dest: 'dist/js/app.js'
 			},
 		},
@@ -141,7 +144,7 @@ module.exports = function (grunt) {
 	};
 	
 	// Full distribution task.
-	grunt.registerTask('dist', ['clean', 'concat', 'copy', 'less', 'autoprefixer', 'csscomb', 'cssmin']);
+	grunt.registerTask('dist', ['clean', 'concat', 'copy', 'uglify', 'less', 'autoprefixer', 'csscomb', 'cssmin']);
 
 	// Task for updating the cached npm packages used by the Travis build (which are controlled by test-infra/npm-shrinkwrap.json).
 	// This task should be run and the updated file should be committed whenever Bootstrap's dependencies change.
